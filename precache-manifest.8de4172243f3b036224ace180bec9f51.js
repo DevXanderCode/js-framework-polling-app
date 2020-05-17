0f84d3cd040ad03750323ffba010c4db8d994dcd
Chinedu Alex <nwekealexchinedu@gmail.com>
2020-05-17 23:22:36 +0100

    Updates

---
+++ precache-manifest.8de4172243f3b036224ace180bec9f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f846a0d09ffdecb7968ca910eba1cddd",
+    "url": "/js-framework-polling-app/index.html"
+  },
+  {
+    "revision": "77ebb384ca3989c94df7",
+    "url": "/js-framework-polling-app/static/css/main.94f6b5f2.chunk.css"
+  },
+  {
+    "revision": "605ed063a558bed45f4b",
+    "url": "/js-framework-polling-app/static/js/2.71c400e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-framework-polling-app/static/js/2.71c400e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ebb384ca3989c94df7",
+    "url": "/js-framework-polling-app/static/js/main.3f4b230d.chunk.js"
+  },
+  {
+    "revision": "dd153d8a257f68351f5b",
+    "url": "/js-framework-polling-app/static/js/runtime-main.411f5879.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/js-framework-polling-app/precache-manifest.8de4172243f3b036224ace180bec9f51.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/js-framework-polling-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
